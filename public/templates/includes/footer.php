cb8ef4e380397d1b9d75ee9f906e1a85ad8204fb
Jassiem Moore <jassiemmoore@gmail.com>
2014-02-19 10:12:31 -0500

    Changed nav/footer color. Removed link stylings. Centered breadcrumbs.

--- public/templates/includes/footer.php
+++ public/templates/includes/footer.php
@@ -1,8 +1,8 @@
 	<div class="footer">
 		<ol class="breadcrumb">
 		  <li><a href="http://www.clemson.edu">Clemson University</a></li>
-		  <li><a href="http://www.clemson.edu/ces/computing/">Clemson School of Computing</a></li>
 		  <li><a href="http://www.clemson.edu/ces"> Clemson College of Engineering and Science</a></li>
+		  <li><a href="http://www.clemson.edu/ces/computing/">Clemson School of Computing</a></li>
 		</ol>
 	</div>
 
